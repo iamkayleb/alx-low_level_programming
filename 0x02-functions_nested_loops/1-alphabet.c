6b39ab7a38fc75a997eaf3d352d21f0873928cfe
Caleb Ogbike <calebogbike@gmail.com>
2022-09-14 21:08:43 -0700

    jsjs

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
-/*
- * main - Prints alphbets in lowercase, followed by a new line
- *
- */
+/**
+* main - Prints alphbets in lowercase, followed by a new line
+*
+*/
 
 void print_alphabet(void)
 {
