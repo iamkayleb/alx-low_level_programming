fb8f10e6cdc4e5bb1fdced8f95c69d678225cb18
Caleb Ogbike <calebogbike@gmail.com>
2022-10-05 05:38:27 -0700

    Update on Tastk 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,26 +1,29 @@
 #include "main.h"
+
 /**
 * str_concat - a function that concatenates two strings.
-* @sl:First string
-* @s2:Second string
+*@s1:First string
+*@s2:Second string
 *
-* Return: NULL in case of failure but pointer to new string in
-* case of success
+*Return: NULL in case of failure , but pointer to new string in
+*case of success
 */
 
 char *str_concat(char *s1, char *s2)
 {
 	char *concat_str;
-	int index, concat_index = 0, len = 0;
+	int index, concat_index = 0,  len = 0;
 
 	if (s1 == NULL)
-		sl = "";
+		s1 = "";
 
 	if (s2 == NULL)
 		s2 = "";
+
 	for (index = 0; s1[index] || s2[index]; index++)
-		1en++;
-	concat_str = malloc(sizeof(char) * 1en);
+		len++;
+
+	concat_str = malloc(sizeof(char) * len);
 
 	if (concat_str == NULL)
 		return (NULL);
@@ -30,5 +33,6 @@ char *str_concat(char *s1, char *s2)
 
 	for (index = 0; s2[index]; index++)
 		concat_str[concat_index++] = s2[index];
+
 	return (concat_str);
 }
